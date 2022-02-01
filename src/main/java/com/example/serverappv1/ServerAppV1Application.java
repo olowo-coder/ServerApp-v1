6ad26b451bd97508d47f0a501b594174e2be2ffd
olowo-coder <olowofela.olanrewaju@decagon.dev>
2022-02-01 13:19:52 +0100

    server

---
+++ src/main/java/com/example/serverappv1/ServerAppV1Application.java
@@ -0,0 +1,13 @@
+package com.example.serverappv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerAppV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerAppV1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/serverappv1/ServerAppV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serverappv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerAppV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
